a4d8657cdcd208ff036689c02c3b5d8d2bac0696
Tony DiNitto <think41c@gmail.com>
2015-03-30 22:49:03 -0600

    More refactoring and cleaning of code.

--- grains.rb
+++ grains.rb
@@ -3,17 +3,12 @@ class Grains
   def square(num_of_square)
     number_to_multiply_grains = 2
     number_to_multiply_grains ** (num_of_square-1)
-
   end
 
   def total
-    # (1..64).collect { |n| square(n) }.reduce(:+)
     total = 0 
     (1..64).each { |x| total += square(x) }
     total
   end
 end
 
-if __FILE__ == $0
-p Grains.new.total
-end
