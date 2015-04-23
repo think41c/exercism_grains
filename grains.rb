4c7001d2895e3fb3d20ec24587b5f4777f3a3f0d
Tony DiNitto <think41c@gmail.com>
2015-04-22 23:49:48 -0400

    Mild refactor.

--- grains.rb
+++ grains.rb
@@ -1,15 +1,11 @@
 class Grains
 
-  def initialize
-  	@squares_on_board = 64
-  end
-
   def square(num_of_square)
     number_to_multiply_grains = 2
     number_to_multiply_grains ** (num_of_square-1)
   end
 
   def total
-    (1..@squares_on_board).reduce { |memo, x| memo += square(x) }
+    (1..64).reduce { |memo, x| memo += square(x) }
   end
 end
